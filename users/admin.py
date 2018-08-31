d075f792df2c97fb1a9149f702ea95772ce0708a
Julian <roussieau.j@gmail.com>
2018-08-31 16:23:56 +0200

    2nd commit

---
+++ users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'treasurer']
+
+    fieldsets = UserAdmin.fieldsets + (
+            ('My fields', {'fields': ('treasurer',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
