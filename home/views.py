2679f530fb8ebdebd2693e94af0ea5fb1dcf67cb
Roussieau <julian.roussieau@student.uclouvain.be>
2018-09-23 16:11:44 +0200

    change home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 # Create your views here.
+@login_required
 def index(request):
-	return render(request, 'index.html', locals())
+	return redirect('supper:planning')
