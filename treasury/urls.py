ef3123bc884868f2adf5b316fe7d08150a44a852
Desausoi Laurent <laurent@desausoi.net>
2019-09-19 17:37:57 +0200

    Debug toolbar

--- treasury/urls.py
+++ treasury/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +24,13 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('', include('home.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
